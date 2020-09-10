917b24b5d94a27fc99d85959996a62a0a6555460
Maxim <purplehorrorrus@gmail.com>
2020-09-10 20:56:38 +0800

    Delete unusded code

--- src/renderer/mixins/other.js
+++ src/renderer/mixins/other.js
@@ -1,10 +1,9 @@
 import { shell } from "electron";
-import fs from "fs";
 import socket from "obs-websocket-js";
 
 export default {
     methods: {
-        checkConnection() {
+        checkConnection () {
             return new Promise(resolve => {
                 const obs = new socket();
                 obs.connect({ address: `${this.address}:${this.port}`, password: this.password })
@@ -12,15 +11,7 @@ export default {
                     .catch(() => resolve({ success: false }));
             });
         },
-        readJSON(dir) {
-            const content = fs.readFileSync(dir, "UTF-8");
-            return JSON.parse(content);
-        },
-        writeJSON(dir, json) {
-            const content = JSON.stringify(json, null, 4);
-            fs.writeFileSync(dir, content);
-        },
-        openLink(url) { 
+        openLink (url) { 
             shell.openExternal(url); 
         }
     }
