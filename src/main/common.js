118b613f30e5d4cdc8e5cdb791421fd06a63b284
Maxim <purplehorrorrus@gmail.com>
2020-12-20 23:47:49 +0800

    Update version

--- src/main/common.js
+++ src/main/common.js
@@ -3,7 +3,7 @@ import { app } from "electron";
 import fs from "fs";
 import path from "path";
 
-app.getVersion = () => "0.3.1";
+app.getVersion = () => "0.3.2";
 app.commandLine.appendSwitch("js-flags", "--expose_gc --max-old-space-size=128");
 
 const icon = path.join("build", "icons", "icon.ico");
