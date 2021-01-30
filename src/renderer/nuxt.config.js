2548484fc87a704f84edcf7cb3a3c1621fe65f99
Maxim <purplehorrorrus@gmail.com>
2021-01-30 18:33:42 +0800

    Change build configuration

--- src/renderer/nuxt.config.js
+++ src/renderer/nuxt.config.js
@@ -1,14 +1,8 @@
 /* eslint-disable no-undef */
-const TerserPlugin = require("terser-webpack-plugin");
-const TerserConfig = {
-    cache: true,
-    parallel: true
-};
-
 const cssnano = require("cssnano");
 
 module.exports = {
-    mode: "spa",
+    mode: "universal",
     head: {
         title: "Stream Overlay",
         meta: [
@@ -56,11 +50,11 @@ module.exports = {
                 }
             });
         },
+        cache: true,
+        parallel: true,
         optimization: {
             minimize: true,
-            minimizer: [
-                compiler => new TerserPlugin(TerserConfig).apply(compiler),
-            ]
+            minimizer: []
         },
         postcss: {
             plugins: [
