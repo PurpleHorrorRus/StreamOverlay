0695f442cf9c3139df61455fa0f1e81343a90bc4
Maxim <purplehorrorrus@gmail.com>
2021-02-11 18:21:23 +0800

    Fix emoticon vertical align

--- src/renderer/components/chat/Body.vue
+++ src/renderer/components/chat/Body.vue
@@ -58,7 +58,7 @@ export default {
             }
 
             .emoticon {
-                vertical-align: top;
+                vertical-align: middle;
             }
 
             .text, .emoticon {
