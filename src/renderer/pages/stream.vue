b25be1b3bb0f6f0547fd567391c199804192fc69
Maxim <purplehorrorrus@gmail.com>
2021-05-10 19:35:08 +0800

    Change style of stream window

--- src/renderer/pages/stream.vue
+++ src/renderer/pages/stream.vue
@@ -1,50 +1,35 @@
 <template>
     <div v-if="!firstLoad" id="modal-stream-container">
-        <div id="modal-stream-content">
-            <div class="modal-title">
+        <div id="modal-stream-content" class="modal-content">
+            <div id="modal-stream-content-title" class="modal-title">
                 <span class="modal-title-text" v-text="'Трансляция'" />
             </div>
-            <div id="game-art">
-                <img class="game-art-image" :src="art">
+            <div id="modal-stream-content-art">
+                <img id="modal-stream-content-art-image" :src="art">
             </div>
-            <div id="edit">
-                <Input 
+            <div id="modal-stream-content-edit">
+                <Input
                     :value="local.title"
-                    :placeholder="'Название трансляции'" 
-                    @input="changeTitle" 
+                    :placeholder="'Название трансляции'"
+                    @input="changeTitle"
                     @keypress.enter.native="update"
                 />
-                <Input 
-                    :value="local.game" 
-                    :placeholder="'Название игры'" 
-                    @input="changeGame" 
+                <Input
+                    :value="local.game"
+                    :placeholder="'Название игры'"
+                    @input="changeGame"
                     @keypress.enter.native="update"
                 />
-                <SolidButton
-                    :label="'Обновить'"
-                    :disabled="disabled"
-                    :load="loading"
-                    @clicked="update"
-                />
+                <SolidButton :label="'Обновить'" :disabled="disabled" :load="loading" @clicked="update" />
             </div>
-        </div>
-        <Recent v-show="recent.length > 0" :recent="recent" />
-        <div id="autocomplete-container">
-            <div id="filtered-top">
-                <Game 
-                    v-for="game of filteredTopGames" 
-                    :key="game.id" 
-                    :game="game" 
-                    @click.native="select(game)" 
-                />
-            </div>
-            <div v-if="search.length > 0" id="search-categories">
-                <Game 
-                    v-for="game of search" 
-                    :key="game.id" 
-                    :game="game" 
-                    @click.native="select(game)" 
-                />
+            <Recent v-show="recent.length > 0" :recent="recent" />
+            <div id="modal-stream-content-games">
+                <div id="modal-stream-content-games-filtered">
+                    <Game v-for="game of filteredTopGames" :key="game.id" :game="game" @click.native="select(game)" />
+                </div>
+                <div v-if="search.length > 0" id="modal-stream-content-games-search">
+                    <Game v-for="game of search" :key="game.id" :game="game" @click.native="select(game)" />
+                </div>
             </div>
         </div>
     </div>
@@ -60,7 +45,7 @@ import SolidButton from "~/components/SolidButton";
 
 import Recent from "~/components/Recent";
 
-const noArt = "https://static-cdn.jtvnw.net/ttv-static/404_boxart-75x115.jpg";
+const noArt = "https://static-cdn.jtvnw.net/ttv-static/404_boxart-{width}x{height}.jpg";
 const artSize = {
     width: 90,
     height: 115
@@ -69,7 +54,7 @@ const artSize = {
 let inputDelay = null;
 
 export default {
-    components: { 
+    components: {
         Game,
         Input,
         SolidButton,
@@ -79,19 +64,18 @@ export default {
     layout: "modal",
     data: () => ({
         firstLoad: true,
-        local: { 
-            title: "", 
+        local: {
+            title: "",
             game: ""
         },
         games: [],
         search: [],
         art: "",
-        topGames: [], 
-        showAutocomplete: false, 
+        topGames: [],
         reset: true,
-        loading: false, 
-        success: false, 
-        error: "" 
+        loading: false,
+        success: false,
+        error: ""
     }),
     computed: {
         ...mapState({
@@ -100,46 +84,36 @@ export default {
             stream: state => state.twitch.stream,
             recent: state => state.config.recent
         }),
-        empty () {
-            return this.resizeArt("https://static-cdn.jtvnw.net/ttv-static/404_boxart-{width}x{height}.jpg");
-        },
-        filteredTopGames () {
+        filteredTopGames() {
             if (!this.local.game.length) {
                 return this.topGames;
             }
 
-            return this.topGames.filter(r => 
-                ~r.name.toLowerCase()
-                    .indexOf(this.local.game.toLowerCase()));
+            return this.topGames.filter(r => ~r.name.toLowerCase().indexOf(this.local.game.toLowerCase()));
         },
-        disabled () {
+        disabled() {
             return this.local.title.length === 0 || this.local.game.length === 0;
         }
     },
     watch: {
-        art: function (newVal) {
+        art: function(newVal) {
             this.art = this.resizeArt(newVal);
         },
-        "local.game": async function (newVal) {
+        "local.game": async function(newVal) {
             // eslint-disable-next-line max-len
-            this.art = this.empty;
+            this.art = this.resizeArt(noArt);
 
             if (inputDelay) {
                 clearTimeout(inputDelay);
                 inputDelay = null;
             }
 
-            if (!this.showAutocomplete) {
-                this.showAutocomplete = true;
-            }
-
             if (newVal.length === 0) {
                 this.search = [];
                 return;
-            } else if (
-                this.findCache(this.games, this.local.game) || 
-                this.findCache(this.search, this.local.game
-                )) return;
+            } else if (this.findCache(this.games, this.local.game) || this.findCache(this.search, this.local.game)) {
+                return;
+            }
 
             inputDelay = setTimeout(async () => {
                 const { data: games } = await this.helix.searchCategories(newVal);
@@ -150,13 +124,13 @@ export default {
             }, 200);
         }
     },
-    async created () {
+    async created() {
         const channel = await this.helix.getChannel(this.user.id);
         this.local = {
             title: channel.status,
             game: channel.game
         };
-        
+
         const game = await this.helix.getGame(this.stream.game);
         this.topGames = await this.helix.getTopGames();
 
@@ -168,33 +142,30 @@ export default {
             updateStream: "twitch/UPDATE",
             saveSettings: "settings/SAVE"
         }),
-        findCache (array, game) {
+        findCache(array, game) {
             const index = array.map(g => g.name).indexOf(game);
-
             if (~index) {
-                const game = array[index];
-                this.showAutocomplete = false;
-                this.art = game.box_art_url;
+                this.art = array[index].box_art_url;
             }
 
             return index !== -1;
         },
-        resizeArt (art) {
-            return art.replace("{width}", artSize.width)
+        resizeArt(art) {
+            return art
+                .replace("{width}", artSize.width)
                 .replace("{height}", artSize.height)
                 .replace("52x72", `${artSize.width}x${artSize.height}`);
         },
-        changeTitle (value) {
+        changeTitle(value) {
             this.local.title = value;
         },
-        changeGame (value) {
+        changeGame(value) {
             this.local.game = value;
         },
-        select (game) {
+        select(game) {
             this.local.game = game.name;
-            setTimeout(() => this.showAutocomplete = false, 300);
         },
-        async update () {
+        async update() {
             if (this.disabled) {
                 return;
             }
@@ -203,9 +174,9 @@ export default {
             this.loading = true;
             this.error = "";
 
-            this.success = await this.updateStream({ 
-                title: this.local.title, 
-                game: this.local.game 
+            this.success = await this.updateStream({
+                title: this.local.title,
+                game: this.local.game
             });
 
             this.loading = false;
@@ -225,65 +196,48 @@ export default {
     left: 50%;
 }
 
-#modal-stream-container {
+#modal-stream-content {
     display: grid;
+    grid-template-columns: 100px 1fr 220px;
+    grid-template-rows: 30px 130px 300px;
+    grid-template-areas:
+        "title title title"
+        "art edit recent"
+        "games games games";
+    grid-gap: 5px;
 
-    grid-template-columns: 600px 300px;
-    grid-template-rows: 150px 300px;
-    grid-template-areas: "content recent"
-                            "games games";
+    &-title {
+        grid-area: title;
+    }
 
-    width: 100%;
+    &-art {
+        grid-area: art;
 
-    #modal-stream-content {
-        grid-area: content;
-        display: grid;
-
-        grid-template-columns: 110px 1fr;
-        grid-template-rows: 1fr;
-        grid-template-areas: 
-            "title title"
-            "art edit";
-
-        .modal-title {
-            grid-area: title;
-        }
-
-        #game-art {
-            grid-area: art;
-
-            padding: 10px;
-
-            &-image {
-                width: 100%;
-                height: 100%;
-            }
-        }
-
-        #edit {
-            grid-area: edit;
-
-            display: flex;
-            justify-content: flex-end;
-            align-content: center;
-            flex-wrap: wrap;
-
-            padding-left: 10px;
-            padding-right: 10px;
+        &-image {
+            width: 100%;
+            height: 100%;
         }
     }
 
-    #autocomplete-container {
+    &-edit {
+        grid-area: edit;
+
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-end;
+    }
+
+    &-games {
         grid-area: games;
 
-        max-height: 300px;
-
         margin-top: 10px;
+        margin-bottom: 10px;
 
+        overflow-x: hidden;
         overflow-y: auto;
 
-        #search-categories {
-            border-top: 1px solid rgb(41, 41, 41);
+        &-search {
+            border-top: 2px solid $outline;
         }
     }
 }
