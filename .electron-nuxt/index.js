023c4bfe33c85014cd307c3338c37895e5986a84
Maxim <purplehorrorrus@gmail.com>
2021-05-10 21:05:30 +0800

    Fix building

--- .electron-nuxt/index.js
+++ .electron-nuxt/index.js
@@ -1,27 +1,27 @@
-const path = require("path")
-const webpack = require("webpack")
-const electron = require("electron")
+const path = require("path");
+const webpack = require("webpack");
+const electron = require("electron");
 
-const { Pipeline, Logger } = require("@xpda-dev/core")
-const { ElectronLauncher } = require("@xpda-dev/electron-launcher")
-const { ElectronBuilder } = require("@xpda-dev/electron-builder")
-const { Webpack } = require("@xpda-dev/webpack-step")
-const resourcesPath = require("./resources-path-provider")
-const { DIST_DIR, MAIN_PROCESS_DIR, SERVER_HOST, SERVER_PORT } = require("./config")
-const NuxtApp = require("./renderer/NuxtApp")
+const { Pipeline, Logger } = require("@xpda-dev/core");
+const { ElectronLauncher } = require("@xpda-dev/electron-launcher");
+const { ElectronBuilder } = require("@xpda-dev/electron-builder");
+const { Webpack } = require("@xpda-dev/webpack-step");
+const resourcesPath = require("./resources-path-provider");
+const { DIST_DIR, MAIN_PROCESS_DIR, SERVER_HOST, SERVER_PORT } = require("./config");
+const NuxtApp = require("./renderer/NuxtApp");
 
-const isDev = process.env.NODE_ENV === "development"
+const isDev = process.env.NODE_ENV === "development";
 
 const launcher = new ElectronLauncher({
     electronPath: electron,
     entryFile: path.join(DIST_DIR, "main/index.js")
-})
+});
 
 const builder = new ElectronBuilder({
     cliOptions: {
         config: path.join(__dirname, "../builder.config.js")
     }
-})
+});
 
 const webpackConfig = Webpack.getBaseConfig({
     entry: path.join(MAIN_PROCESS_DIR, "index.js"),
@@ -35,15 +35,15 @@ const webpackConfig = Webpack.getBaseConfig({
             "process.env.DEV_SERVER_URL": `"${SERVER_HOST}:${SERVER_PORT}"`
         })
     ]
-})
+});
 
 const webpackMain = new Webpack({
     logger: new Logger("Main", "olive"),
     webpackConfig,
     launcher // need to restart launcher after compilation
-})
+});
 
-const nuxt = new NuxtApp(new Logger("Nuxt", "green"))
+const nuxt = new NuxtApp(new Logger("Nuxt", "green"));
 
 const pipe = new Pipeline({
     title: "Electron-nuxt",
@@ -51,6 +51,6 @@ const pipe = new Pipeline({
     steps: [webpackMain, nuxt],
     launcher,
     builder
-})
+});
 
-pipe.run()
+pipe.run();
