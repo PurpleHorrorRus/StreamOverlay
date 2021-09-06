b1efe4609ca31aba7c2c50eda795e1c540ec52a0
Maxim <purplehorrorrus@gmail.com>
2021-09-07 00:46:31 +0800

    Fix time formatting

--- src/renderer/plugins/misc.js
+++ src/renderer/plugins/misc.js
@@ -6,15 +6,16 @@ export default {
         return !params.raw ? await response.json() : await response.text();
     },
     formatTime: time => {
-        let h = time.hours,
-            m = time.mins,
-            s = time.seconds;
+        let { hours: h, mins: m, seconds: s } = time;
+
         if (time.seconds < 10) s = `0${time.seconds}`;
         if (time.mins < 10) m = `0${time.mins}`;
-        if (time.hours) {
+        if (time.hours !== undefined) {
             if (time.hours < 10) h = `0${time.hours}`;
             return `${h}:${m}:${s}`;
         }
+
+        return `${m}:${s}`;
     },
     array_move(arr, oldIndex, newIndex) {
         if (newIndex >= arr.length) {
