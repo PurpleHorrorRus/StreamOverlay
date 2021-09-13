578db88ad06f4cd585314db98ef2c5f1d5cd1ce6
PurpleHorrorRus <purplehorrorrus@gmail.com>
2021-09-13 23:58:30 +0800

    Fix code style

--- src/renderer/components/obs/TechInfo.vue
+++ src/renderer/components/obs/TechInfo.vue
@@ -1,43 +1,13 @@
 <template>
-    <Movable
-        :source="settings.TechInfo"
-        :name="'Техническая информация'"
-        @onDrag="onDrag"
-        @onResize="onResize"
-    >
+    <Movable :source="settings.TechInfo" :name="'Техническая информация'" @onDrag="onDrag" @onResize="onResize">
         <div id="tech-info">
-            <TechInfoItem
-                v-if="status.stream"
-                :label="'Bitrate'"
-                :value="formatBitrate"
-                :valueStyle="bitrateStyle"
-            />
-            <TechInfoItem
-                :label="'FPS'"
-                :value="formatFPS"
-                :valueStyle="FPSStyle"
-            />
-            <TechInfoItem
-                :label="'Frametime'"
-                :value="formatFrametime"
-            />
-            <TechInfoItem
-                :label="'Encoding'"
-                :value="formatEncoding"
-            />
-            <TechInfoItem
-                v-if="status.stream || status.recording"
-                :label="'Network'"
-                :value="formatNetwork"
-            />
-            <TechInfoItem
-                :label="'CPU Usage'"
-                :value="formatCPU"
-            />
-            <TechInfoItem
-                :label="'Memory Usage'"
-                :value="formatMemory"
-            />
+            <TechInfoItem v-if="status.stream" :label="'Bitrate'" :value="formatBitrate" :valueStyle="bitrateStyle" />
+            <TechInfoItem :label="'FPS'" :value="formatFPS" :valueStyle="FPSStyle" />
+            <TechInfoItem :label="'Frametime'" :value="formatFrametime" />
+            <TechInfoItem :label="'Encoding'" :value="formatEncoding" />
+            <TechInfoItem v-if="status.stream || status.recording" :label="'Network'" :value="formatNetwork" />
+            <TechInfoItem :label="'CPU Usage'" :value="formatCPU" />
+            <TechInfoItem :label="'Memory Usage'" :value="formatMemory" />
         </div>
     </Movable>
 </template>
@@ -58,35 +28,33 @@ export default {
             settings: state => state.settings.settings,
             status: state => state.obs.status
         }),
-        formatBitrate () {
+        formatBitrate() {
             return `${(this.status.bitrate / 1024).toFixed(1)} MB/s`;
         },
-        bitrateStyle () {
+        bitrateStyle() {
             return {
                 color: this.status.bitrate < 200 ? "red" : "#fff"
             };
         },
-        formatFPS () {
+        formatFPS() {
             return this.status.tech.fps.toFixed(0);
         },
-        formatFrametime () {
+        formatFrametime() {
             return `${this.status.tech["average-frame-time"].toFixed(2)} ms`;
         },
-        formatEncoding () {
+        formatEncoding() {
             return `${this.status.tech["render-missed-frames"]} / ${this.status.tech["render-total-frames"]}`;
         },
-        formatNetwork () {
+        formatNetwork() {
             return `${this.status.tech["output-skipped-frames"]} / ${this.status.tech["output-total-frames"]}`;
         },
-        formatCPU () {
-            return this.status.tech["cpu-usage"]
-                ? this.status.tech["cpu-usage"].toFixed(2) + "%"
-                : "-";
+        formatCPU() {
+            return this.status.tech["cpu-usage"] ? this.status.tech["cpu-usage"].toFixed(2) + "%" : "-";
         },
-        formatMemory () {
-            return `${this.status.tech["memory-usage"].toFixed(1)} MB`;  
+        formatMemory() {
+            return `${this.status.tech["memory-usage"].toFixed(1)} MB`;
         },
-        FPSStyle () {
+        FPSStyle() {
             return {
                 color: this.status.tech.fps < this.status.videoSettings.fps ? "red" : "#fff"
             };
@@ -96,13 +64,13 @@ export default {
         ...mapActions({
             saveSettings: "settings/SAVE"
         }),
-        onResize (x, y, width, height) {
+        onResize(x, y, width, height) {
             this.settings.TechInfo.width = width;
             this.settings.TechInfo.height = height;
-            
+
             this.onDrag(x, y);
         },
-        onDrag (x, y) {
+        onDrag(x, y) {
             this.settings.TechInfo.x = x;
             this.settings.TechInfo.y = y;
             this.save();
