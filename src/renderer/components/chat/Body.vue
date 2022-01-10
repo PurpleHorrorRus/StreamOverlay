68ac749a5c27dd3e6a9cf96b9ce57a5ec45c8b73
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-01-10 23:37:04 +0800

    Support for links
    Click to open in default browser

--- src/renderer/components/chat/Body.vue
+++ src/renderer/components/chat/Body.vue
@@ -1,16 +1,43 @@
 <template>
     <div class="items">
         <div v-for="(item, index) of items" :key="index" class="item">
-            <span v-if="item.type === 'text'" :style="textStyle" class="text stroke" v-text="item.content" />
-            <img v-else-if="item.type === 'emoji'" class="emoticon" :style="emoticionStyle" :src="item.content">
+            <span
+                v-if="item.type === 'text'"
+                :style="textStyle"
+                class="text stroke"
+                v-text="item.content"
+            />
+
+            <span
+                v-else-if="item.type === 'link'"
+                v-tooltip="'Нажмите, чтобы перейти по ссылке'"
+                :style="textStyle"
+                class="text link stroke"
+                @click="openLink(item)"
+                v-text="linkText(item)"
+            />
+
+            <img
+                v-else-if="item.type === 'emoji'"
+                class="emoticon"
+                :style="emoticionStyle"
+                :src="item.content"
+            >
         </div>
     </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { shell } from "electron";
+
+import MessageMixin from "~/components/chat/mixin";
+
+// eslint-disable-next-line max-len
+const linkRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+const domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/;
 
 export default {
+    mixins: [MessageMixin],
     props: {
         items: {
             type: Array,
@@ -18,18 +45,18 @@ export default {
         }
     },
     computed: {
-        ...mapState({
-            settings: state => state.settings.settings
-        }),
-        textStyle() {
-            return {
-                fontSize: `${this.settings.chat.font}pt`
-            };
-        },
         emoticionStyle() {
             return {
-                height: `${this.settings.chat.font - 1}pt`
+                height: `${this.settings.chat.font + 2}pt`
             };
+        }
+    },
+    methods: {
+        linkText(item) {
+            return item.content.replace(linkRegex, `[${item.content.match(domainRegex)[1]}]`);
+        },
+        openLink(item) {
+            shell.openExternal(item.content);
         }
     }
 };
@@ -53,9 +80,17 @@ export default {
             .text {
                 font-family: "Roboto Condensed";
                 font-weight: bold;
+
+                &.link {
+                    color: #0077ff;
+                    cursor: pointer;
+                }
             }
 
             .emoticon {
+                position: relative;
+                bottom: 1px;
+
                 vertical-align: middle;
             }
 
