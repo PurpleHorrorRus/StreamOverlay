3fa20f4ab4b585a54f866ffc1ea1f0f5f779e777
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-03-26 22:32:08 +0800

    Trovo: Improve formatting messages content

--- src/renderer/store/services/trovo.js
+++ src/renderer/store/services/trovo.js
@@ -8,6 +8,7 @@ import misc from "~/plugins/misc";
 
 // eslint-disable-next-line max-len
 const linkRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+// eslint-disable-next-line no-useless-escape
 const domainRegex = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/;
 
 const notifications = {
@@ -20,18 +21,18 @@ const notifications = {
 };
 
 const addMessagePart = (formatted, type, content) => {
-    if (content.length > 0) {
-        return [...formatted, { 
-            type, 
-            content: content.trim() 
-        }];
+    if (typeof content === "string") {
+        content = content.trim();
+        if (content.length === 0) return formatted;
     }
 
+    formatted.push({ type, content });
     return formatted;
 };
 
 export default {
     namespaced: true,
+
     actions: {
         INIT: async ({ dispatch, rootState }) => {
             let client = new TrovoAPI({
@@ -147,58 +148,46 @@ export default {
         },
 
         FORMAT_MESSAGE: async ({ dispatch }, message) => {
-            let formatted = [];
+            const formatted = [];
             let part = "";
 
-            const splitted = message.split(" ");
+            const splitted = message
+                .replaceAll("https://", "www.")
+                .split(/(?=[ :])/g).map(w => w.trim())
+                .filter(w => w.length > 0);
+
             for (let index in splitted) {
                 index = Number(index);
                 const word = splitted[index];
 
-                if (linkRegex.test(word)) {
-                    if (part.length > 0) {
-                        formatted = addMessagePart(formatted, "text", part);
-                        part = "";
-                    }
-                    
-                    const domain = word.match(domainRegex)[1];
-                    formatted = addMessagePart(formatted, "link", domain);
+                if (linkRegex.test(word)) { // Format link
+                    addMessagePart(formatted, "text", part);
+                    part = "";
+
+                    addMessagePart(formatted, "link", { 
+                        domain: word.match(domainRegex)[1].replace("www.", ""), 
+                        link: word.replace("www.", "https://")
+                    });
+
                     continue;
                 }
 
-                if (!~word.indexOf(":")) {
-                    if (word) {
-                        part += word + " ";
-                    }
-                } else {
-                    const wordSplitted = word.split(":");
-
-                    for (const emoteKey of wordSplitted) {
-                        if (emoteKey.length > 0) {
-                            const emote = await dispatch("emotes/FIND_EMOTE", emoteKey);
-                            if (emote) {
-                                if (part.length > 0) {
-                                    formatted = addMessagePart(formatted, "text", part);
-                                    part = "";
-                                }
-
-                                formatted = addMessagePart(formatted, "emote", emote.gifp || emote.url);
-                            } else part += ":" + emoteKey;
-                        } else part += " ";
-                    }
-                }
-                
-                if (index === splitted.length - 1) {
-                    if (part.length > 0) {
-                        formatted = addMessagePart(formatted, "text", part);
-                        part = "";
-                    }
+                if (~word.indexOf(":")) {
+                    const emote = await dispatch("emotes/FIND_EMOTE", word); // Format Emote
                     
-                    return formatted;
+                    if (emote) {
+                        addMessagePart(formatted, "text", part);
+                        part = "";
+    
+                        addMessagePart(formatted, "emote", emote.gifp || emote.url);
+                        continue;
+                    }
                 }
+
+                part += word + " ";
             }
 
-            return formatted;
+            return addMessagePart(formatted, "text", part);
         },
 
         FORMAT_MESSAGE_TIME: (_, message) => {
