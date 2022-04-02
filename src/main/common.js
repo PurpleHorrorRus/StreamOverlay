ea17e198f66c97116fa8effe411a0c6faf1a51f7
PurpleHorrorRus <purplehorrorrus@gmail.com>
2022-04-02 18:15:23 +0800

    Update version

--- src/main/common.js
+++ src/main/common.js
@@ -5,7 +5,7 @@ import path from "path";
 import storage from "./storage";
 import windows from "./windows";
 
-app.getVersion = () => "1.6.2";
+app.getVersion = () => "1.6.3";
 
 const icon = path.join("build", "icons", "icon.ico");
 const isDev = process.env.NODE_ENV === "development";
