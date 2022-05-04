eafb4ebe54db21cd44b2421044dbc59bbc32e672
Maxim <purplehorrorrus@gmail.com>
2022-05-04 20:03:40 +0800

    Fix code style

--- src/main/common.js
+++ src/main/common.js
@@ -7,12 +7,9 @@ import WindowsLogic from "./windows";
 
 app.getVersion = () => "1.7.0";
 
-const icon = path.join("build", "icons", "icon.ico");
-const isDev = process.env.NODE_ENV === "development";
-
 export default {
-    icon,
-    isDev,
+    icon: path.join("build", "icons", "icon.ico"),
+    isDev: process.env.NODE_ENV === "development",
     storage,
     windows: new WindowsLogic()
 };
