f77b31bc465e23d734965856650cbd42236df52e
Maxim <purplehorrorrus@gmail.com>
2022-09-01 14:43:46 +0800

    Patch version

--- src/main/common.js
+++ src/main/common.js
@@ -5,7 +5,7 @@ import path from "path";
 import storage from "./storage";
 import WindowsLogic from "./windows";
 
-app.getVersion = () => "1.8.1";
+app.getVersion = () => "2.0.0";
 
 export default {
     icon: path.join("build", "icons", "icon.ico"),
