52da36aabafb35ed63a3a3eb25a3782f6f5e06ff
Maxim <purplehorrorrus@gmail.com>
2022-11-23 16:46:51 +0800

    Register new global components

--- src/renderer/plugins/globalComponents.js
+++ src/renderer/plugins/globalComponents.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
 
 const components = {
-    Title: () => import("~/components/Menu/Title"), 
-    Input: () => import("~/components/Settings/Input"),
-    ToggleButton: () => import("~/components/Settings/Item"),
-    Range: () => import("~/components/Settings/Range"),
-    SolidButton: () => import("~/components/SolidButton"),
-    Movable: () => import("~/components/Movable"),
+    Title: () => import("~/components/Menu/Title.vue"),
+    MenuLink: () => import("~/components/Menu/Link.vue"),
+    ModalCategory: () => import("~/components/Settings/Category.vue"),
+    Input: () => import("~/components/Settings/Input.vue"),
+    ToggleButton: () => import("~/components/Settings/Item.vue"),
+    Range: () => import("~/components/Settings/Range.vue"),
+    SolidButton: () => import("~/components/SolidButton.vue"),
+    Movable: () => import("~/components/Movable.vue"),
 
-    LoaderIcon: () => import("~/assets/icons/loader.svg")
+    LoaderIcon: () => import("~icons/loader.svg")
 };
 
 Object.entries(components).forEach(([name, component]) => {
